0ae96e698ecfe7e93543166a720f9e20f32ad31d
adri4nobritow <adrianobritow@gmail.com>
2024-08-12 20:46:40 -0300

    Update app.json with dynamic url for the iframe modal

--- src/app/extensions/DisplayIframe.jsx
+++ src/app/extensions/DisplayIframe.jsx
@@ -1,38 +1,38 @@
-import { Link, Button, Text, Box, Flex, hubspot } from "@hubspot/ui-extensions";
+import { hubspot, Button, Flex } from "@hubspot/ui-extensions";
 
-// Define the extension to be run within the Hubspot CRM
-hubspot.extend((
-  { actions } // serverless function is not required for simply displaying an iframe
-) => <Extension openIframe={actions.openIframeModal} />);
+// Define the extension to be run within the HubSpot CRM
+hubspot.extend(({ crm, actions }) => {
+  // Fetch the ticket properties, including 'cms_org_id'
+  crm.fetch('ticket').then(ticket => {
+    const cmsOrgId = ticket.properties['cms_org_id']; // Get the 'cms_org_id' property
 
-// Define the Extension component, taking in openIframe as a prop
-const Extension = ({ openIframe }) => {
-  const handleClick = () => {
-    openIframe({
-      uri: "https://wikipedia.com/", // this is a relative link. Some links will be blocked since they don't allow iframing
-      height: 1000,
-      width: 1000,
-    });
-  };
+    // If the property exists, open the iframe with the dynamic URL
+    if (cmsOrgId) {
+      const handleClick = () => {
+        actions.openIframeModal({
+          uri: `https://admin.prod.jeeves.vpn/admin/organizations/${cmsOrgId}`,
+          height: 1000,
+          width: 1000,
+        });
+      };
 
-  return (
-    <>
-      <Flex direction="column" align="start" gap="medium">
-        <Text>
-          Clicking the button will open a modal dialog with an iframe that
-          displays the content at the provided URL. Get more info on how to do
-          this {" "}
-          <Link href="https://developers.hubspot.com/docs/platform/create-ui-extensions#open-an-iframe-in-a-modal">
-            here
-          </Link>
-        </Text>
-
-        <Box>
+      // Render the button that triggers the modal
+      return (
+        <Flex direction="column" align="start" gap="medium">
           <Button type="submit" onClick={handleClick}>
-            Click me
+            Open Organization Admin
           </Button>
-        </Box>
-      </Flex>
-    </>
-  );
-};
+        </Flex>
+      );
+    } else {
+      // Handle case where 'cms_org_id' is not available
+      return (
+        <Flex direction="column" align="start" gap="medium">
+          <Button type="submit" disabled>
+            cms_org_id not found
+          </Button>
+        </Flex>
+      );
+    }
+  });
+});
